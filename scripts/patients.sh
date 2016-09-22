ecd41f166304ccf1c91a1a899a2b3f309413b9b0
styletab <katherinetab@gmail.com>
2016-09-22 11:51:21 -0400

    Full feature for POST to /patients
    Allow POST requests to /patients to filter to the #create memthod
    in the PatientsControllers. This full feature also includes a curl
    script for testing the POST request.

---
+++ scripts/patients.sh
@@ -0,0 +1,18 @@
+# index
+
+
+
+#show
+
+
+
+#create
+
+curl --include --request POST http://localhost:3000/patients \
+  --header "Content-Type: application/json" \
+  --data '{
+    "patient": {
+      "name": "Will",
+      "sickness": "an example sickness"
+    }
+  }'
